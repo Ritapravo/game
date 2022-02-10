b625b549b300ab56c5738178128c0446e51dd889
ritapravo <ritopravosarker@gmail.com>
2022-02-10 19:11:38 +0530

    Thursday 10 February 2022 07:11:36 PM IST

---
+++ src/components/Coin.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { useState } from "react";
+import './Board.css'
+import { x_val, y_val } from "../functions";
+
+
+const Coin = (props) => {
+
+    var speed = props.speed;
+    var z = props.position + props.diceVal;
+    const [counter, setCounter] = useState(props.position);
+    const [moveOn, setMoveOn] = useState(false);
+
+    React.useEffect(() => {
+        let interval = null;
+        if (moveOn) {
+            interval = setInterval(() => {
+                setCounter((prevCounter) => prevCounter + 1);
+            }, speed);
+        }
+        else {
+            clearInterval(interval);
+            props.move(setCounter, props.coin.id);
+        }
+        return () => {
+            clearInterval(interval);
+        };
+    }, [moveOn]);
+
+    if (moveOn && counter === z) {        
+        setMoveOn(false);
+    }
+
+    const slide = () => {
+        if (z > 100) { props.move(setCounter, props.coin.id); return; }
+        
+        if (props.turn && props.enterEnabled)
+            setMoveOn(true);
+    }
+
+    const Coin_marker = {
+        margin: props.coin.margin,
+        backgroundColor: props.coin.backgroundColor,
+        borderRadius: '80% 80% 0% 80%',
+        transform: "rotate(45deg)",
+        border: '1px solid rgb(0, 0, 0)',
+        gridRowStart: x_val(counter),
+        gridColumnStart: y_val(counter),
+        // transition: "all 2s ease-in"
+    };
+
+
+    return (
+        <div
+            style={Coin_marker}
+            className={props.turn && props.enterEnabled ? "blinker" : ""}
+            onClick={slide}
+        >
+            <div className="circle"/>
+        </div>
+    );
+}
+export default Coin;
